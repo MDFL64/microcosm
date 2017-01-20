0b915c840f4b6d4d8c1665b3f83f9b445122fb7d
real name genij <zgxzgx@gmail.com>
2017-01-19 19:40:09 -0600

    Teleporter Done
    phew, it works!... not usable with the ufo though :\

--- gamemodes/microcosm/entities/entities/micro_comp_teleporter.lua
+++ gamemodes/microcosm/entities/entities/micro_comp_teleporter.lua
@@ -1,7 +1,5 @@
 --[[
-todo:	(Help me) There is an error when you're not on a team and have not yet joined each team.
-			To get around this you have to join each team once.
-		(Help me) Make it so teleporter sound only plays once
+todo:	Teleporter will not work when going between two different ship types. id est std -> ufo
 		add teleporter animation?  Like, a bunch of blue text that fills up its client display box.
 		(We, in sh_shipinfo.lua) add location for UFO? Normal one could be:
 		local tele = ents.Create("micro_comp_teleporter")
@@ -20,12 +18,18 @@ ENT.ComponentScreenHeight = 100 --90
 ENT.ComponentScreenOffset = Vector(2,-27,55)
 ENT.ComponentScreenRotation = Angle(0,90,90)
 
-local sound_in_range = Sound("npc/overwatch/radiovoice/preparetoreceiveverdict.wav")
+local sound_in_range = Sound("npc/overwatch/radiovoice/preparetoreceiveverdict.wav", 125)
+local sound_tele_use = Sound("ambient/machines/teleport1.wav")
 
 --variables to change for gameplay
 local max_teleport_distance = 125
-local teleporter_origin_fix = Vector(0,0,-128) --vector to be added to the origin of the teleporter's prop. Set manually :\
+local teleporter_origin_fix = Vector(0,0,-48) --vector to be added to the origin of the teleporter's prop. Set manually :\
+--Under ENT:Initialize() self.teleporter_position_relative_to_the_ship is hardcoded. It's the position of the teleporter relative to the origin and is the reason why this won't work with the ufo
 --end variables to change for gameplay
+
+function ENT:SetupDataTables()
+	self:NetworkVar("Bool", 0, "InRange")
+end
 
 function ENT:GetComponentName()
 	return "Teleporter"
@@ -33,48 +37,31 @@ end
 
 function ENT:Initialize()
 	BaseClass.Initialize(self)
-	self.which_tele_is_closest = 0
 	self.teleporter_distance = 0
 	self.teleporter_distance_closest = 9999999
-	self.is_within_range = false
-	self.is_Home = true
 	self.teleporter_guide_of_closest = {}
 	for i=1,#MICRO_SHIP_INFO do
-    	self.teleporter_guide_of_closest[i] = 0
+		self.teleporter_guide_of_closest[i] = 0
 	end
-	local teleporter_entid = {}
-	for i=1,#MICRO_SHIP_INFO do
-    	teleporter_entid[i] = self:EntIndex()
-    end
-	local teleporter_pos = {}
-    for i=1,#MICRO_SHIP_INFO do --i to be the ent index
-    	teleporter_pos[i] = Vector(0,0,0)
-    end
-	--self:EntIndex()
-	--self:GetPos()
-	local teleporter_info = {}
-    for i=1,#MICRO_SHIP_INFO do --i will be ship number, j will be entid, result will be teleporter position
-		teleporter_info[i] = {}
-		for j=1,#MICRO_SHIP_INFO do
-			teleporter_info[i][j] = Vector(0,0,0)
-		end
-    end
-
+	self.teamo = 0
+	self.sound_has_played = false
+	self.teleporter_position_relative_to_the_ship = Vector(-200,-225,0) --changed later, don't worry
 end
 
 function ENT:Use(ply)
 	if not self:IsBroken() then
-		print("It's ja boy's team! ".. ply:Team())
-		if not is_Home then
-			--maybe get each other teleporters in an array and get that one's position instead
-			ply:SetPos(self.teleporter_pos[ply:Team()]+teleporter_origin_fix)
-			is_Home = true
-			print("TELEPORT HOMEO")
-		elseif is_Home then
-			print("TELESNORT AWAYWAYS ".. self.teleporter_pos[self.teleporter_guide_of_closest[ply:Team()]])
-			ply:SetPos(self.teleporter_pos[self.teleporter_guide_of_closest[ply:Team()]]+teleporter_origin_fix)
-			--ply:SetPos(MICRO_SHIP_INFO[self.teleporter_guide_of_closest[ply:Team()]].origin)
-			is_Home = false
+		--easy way to get the team the teleporter is on
+		for i=1,#MICRO_SHIP_INFO do
+			if self:GetShipInfo() == MICRO_SHIP_INFO[i] then
+				self.teamo = i
+			end
+		end
+
+		if self.teleporter_guide_of_closest[self.teamo] != 0 then
+			--print("TELESNORT AWAYWAYS ".. self.teleporter_guide_of_closest[self.teamo])
+			ply:SetEyeAngles(ply:GetShootPos():Angle()*-1) --I know, I know, it doesn't work well.
+			ply:SetPos(MICRO_SHIP_INFO[self.teleporter_guide_of_closest[self.teamo]].origin + self.teleporter_position_relative_to_the_ship + teleporter_origin_fix)
+			ply:EmitSound(sound_tele_use)
 		end
 	end
 end
@@ -83,51 +70,46 @@ function ENT:Think()
 	--get smallest distance away
 	--7:17 AM - 0x5f3759df: you can use ent:GetShipInfo()
 	--7:18 AM - 0x5f3759df: to get the ship info a player or entity is on
-	
 	if CLIENT then return end
 
-	teleporter_distance_closest = 9999999
-	--print("cancer cycle, start ************************************************************************************")
+	self.teleporter_distance_closest = 9999999
 	for t=1,#MICRO_SHIP_INFO do --is really team number, hence t
 		for i=1,#MICRO_SHIP_INFO do
-			teleporter_distance = MICRO_SHIP_INFO[t].entity:GetPos():Distance(MICRO_SHIP_INFO[i].entity:GetPos()) --derps if you do not go to each ship.
-			if teleporter_distance > 0 then --way easier than getting team number xD
-				if teleporter_distance < max_teleport_distance && teleporter_distance <= teleporter_distance_closest then
-					teleporter_distance_closest = teleporter_distance
+			self.teleporter_distance = MICRO_SHIP_INFO[t].entity:GetPos():Distance(MICRO_SHIP_INFO[i].entity:GetPos())
+			if t != i then
+				if self.teleporter_distance < max_teleport_distance && self.teleporter_distance <= self.teleporter_distance_closest then
+					self.teleporter_distance_closest = self.teleporter_distance
 					--print("telepoter distance closet "..teleporter_distance_closest)
 					self.teleporter_guide_of_closest[t] = i
-					is_within_range = true
+					self:SetInRange(true)
 					--print("tele guide info ".. self.teleporter_guide_of_closest[t])
-				elseif is_within_range && teleporter_distance_closest >= max_teleport_distance then
-					is_within_range = false
+				elseif self.teleporter_distance_closest >= max_teleport_distance then
 					self.teleporter_guide_of_closest[t] = 0
+				end
+				if self:GetInRange() && self.teleporter_distance_closest >= max_teleport_distance then
+					self:SetInRange(false)
+					self.sound_has_played =  false
 				end
 			end
 		end
 	end
-	--probably want to change or remove this if statement entirely. it makes it so the sound continously plays when in range...
-	if teleporter_distance_closest <= max_teleport_distance then --use a flag? another instance variable of the entity
+	if self:GetInRange() && not self.sound_has_played then
 		self:EmitSound(sound_in_range)
+		self.sound_has_played = true
 	end
 end
 
-function ENT:isInRange()
-	--print("IS IN RANGE CHECEK>>>>".. teleporter_distance_closest)
-	--return teleporter_distance_closest <= max_teleport_distance
-
-end
-
-if CLIENT then --make network variable for isInRange, you can get and set it automatically!
+if CLIENT then
 	function ENT:GetScreenText()
 		local ship = self:GetShipInfo().entity
 		local hurt = self:IsBroken()
 
 		if hurt then
-			return "Genij place telepoty?",Color(255,0,255)
+			return "Genij telepoty?",Color(255,0,255)
+		elseif self:GetInRange() then
+			return "In Range",Color(0,255,0)
 		elseif ship:GetIsHome() then
 			return "Home",Color(0,255,0)
-		elseif self:isInRange() then
-			return "In Range",Color(0,255,0)
 		else
 			return "Not In Range",Color(255,0,0)
 		end
